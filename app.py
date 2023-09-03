bdcc1db8623e4d6e80e352ddb58347bfcbe62d3b
EvelinaSarg <126480724+EvelinaSarg@users.noreply.github.com>
2023-09-03 23:57:49 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,70 @@
+
+import random
+import string   
+import logging
+from flask import Flask, send_file, request, redirect, render_template
+from scrapinghub import ScrapinghubClient
+
+
+app = Flask(__name__)
+
+if __name__ != '__main__': 
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
+def generate_random_string(length):
+    letters = string.ascii_letters
+    return ''.join(random.choice(letters) for _ in range(length))
+
+@app.route('/')
+def index():
+    return send_file('index.html')
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+    #return 'Your job has been successfully submitted! You will receive an email when it is complete.' #The check may take a few seconds to be completed and for your report to be ready. 
+
+
+@app.route('/', methods=['POST'])
+def startJob():
+    apiKey = 'd4f6b6b9d7cb420d82870f6b2115def5'
+    client = ScrapinghubClient(apiKey)
+    project = client.get_project(664878)
+    data = request.form
+    app.logger.info(data)
+    job_args = {
+        'url': data['url'],
+        'check_images': data['images']=='on',
+        'check_fragments': data['fragments']=='on',
+        'report_path': f'{generate_random_string(10)}',
+        'email': data['email']
+    }
+    job_settings = {
+        'CLOSESPIDER_PAGECOUNT': data['pages'],
+        'CLOSESPIDER_TIMEOUT': data['timeout'],
+        'DEPTH_LIMIT': data['depth'],
+        'ENABLE_STATUS_PIPELINE': data.get('status') != 'on',
+        'LOG_LEVEL': 'DEBUG',
+        'DEPTH_PRIORITY': 0 if data['search_type']=='BFS' else 1,
+        'CONCURRENT_REQUESTS': data['concurrent'],  
+        'CONCURRENT_REQUESTS_PER_DOMAIN': data['concurrent'],
+        'CONCURRENT_REQUESTS_PER_IP': data['concurrent'],
+        'REDIRECT_ENABLED': data.get('redirects') == 'on'
+    }
+
+
+    job = project.jobs.run('checker', job_args=job_args, job_settings=job_settings,
+                       priority=2, units=1)
+    
+
+
+    return redirect('/success')
+   
+        
+   
+if __name__ == '__main__':
+    app.run()
+    
+
